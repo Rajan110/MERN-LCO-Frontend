20f7cfaaceadb1ff3a61a93973ed2c891d8e5d17
Rajan <rajangundigara@gmail.com>
2021-04-15 01:48:23 +0530

    Implemented Sign-Up Functionality

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,8 +1,49 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (param) => (event) => {
+    setValues({ ...values, error: false, [param]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          let errMsg = "";
+          Array.from(data.error.error).forEach((err) => {
+            errMsg += "\n" + err.msg;
+          });
+
+          setValues({ ...values, error: errMsg, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -10,20 +51,66 @@ const Signup = () => {
           <form action="">
             <div className="form-group">
               <label className="text-light">Name</label>
-              <input className="form-control" type="text"></input>
+              <input
+                onChange={handleChange("name")}
+                className="form-control"
+                type="text"
+                value={name}
+              ></input>
             </div>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email"></input>
+              <input
+                onChange={handleChange("email")}
+                className="form-control"
+                type="email"
+                value={email}
+              ></input>
             </div>
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password"></input>
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+                value={password}
+              ></input>
             </div>
-            <button className="btn btn-success btn-block">
+            <button onClick={onSubmit} className="btn btn-success btn-block">
               Submit
             </button>
           </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            Account Created Successfully. Please{" "}
+            <Link to="/signin">Login Here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            <p style={{ whiteSpace: "pre" }}>{error}</p>
+          </div>
         </div>
       </div>
     );
@@ -34,7 +121,10 @@ const Signup = () => {
       title="Sign Up"
       description="Please Fill Below Form To SignUp / Register."
     >
+      {successMessage()}
+      {errorMessage()}
       {signUpForm()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
     </Base>
   );
 };
